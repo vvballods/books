737d27f7e51f68ef81475ce8998134b28f35c56e
Valters Ballods <vv.ballods@bilderlings.com>
2021-12-18 11:05:18 +0200

    added junit dependency

---
+++ src/test/java/lv/sda/books/ApplicationTest.java
@@ -0,0 +1,13 @@
+package lv.sda.books;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ApplicationTest {
+
+    @Test
+    public void sampleTest() {
+        assertEquals(1, 1);
+    }
+}
