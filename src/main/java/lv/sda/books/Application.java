2d4a6b81aa98bd7b45192fc398e5964fba1a09d5
Valters Ballods <vv.ballods@bilderlings.com>
2021-12-18 13:56:26 +0200

    18.12.2021. initial menu

--- src/main/java/lv/sda/books/Application.java
+++ src/main/java/lv/sda/books/Application.java
@@ -8,14 +8,43 @@ public class Application {
 
     public static void main(String[] args) {
         while (true) {
-            System.out.println("Ievadiet q, lai izietu.");
-            System.out.println("Ievadiet ciparu:");
+            System.out.println("Menu");
+            System.out.println("1. Search Book");
+            System.out.println("2. Add Book");
+            System.out.println("3. Remove Book");
+            System.out.println("4. Get book info");
+            System.out.println("5. List Available Books");
+            System.out.println(" To quit press: q");
             String input = scanner.nextLine();
 
             if ("q".equalsIgnoreCase(input)) {
-                System.out.println("Izejam, beidzam darbu.");
+                System.out.println("Quiting application");
                 break;
             }
+            switch (input) {
+                case "1":
+                    System.out.println("Searching for book");
+                    break;
+                case "2":
+                    System.out.println("Adding book");
+
+                    break;
+                case "3":
+                    System.out.println("Removing book");
+                    System.out.println("Provide Isbn of the book to remove: ");
+                    scanner.nextLine();
+                    break;
+                case "4":
+                    System.out.println("Getting book info");
+                    break;
+                case "5":
+                    System.out.println("Listing available books:");
+                    break;
+                default:
+                    System.out.println("Wrong input, please try again");
+            }
+            System.out.println();
+
         }
     }
 }
